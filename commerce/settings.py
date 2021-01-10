d3607afd9da6547ed46d1a6f12e3da4e35810397
Angel Rodriguez <angelr1076@gmail.com>
2021-01-10 17:22:48 -0500

    updated static root

--- commerce/settings.py
+++ commerce/settings.py
@@ -133,11 +133,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# For images in ckeditor
 MEDIA_URL = '/media/'
 
 
